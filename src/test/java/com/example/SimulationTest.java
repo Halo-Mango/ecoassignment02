f46a1c2b4c6ed05e028522cf1ac3b6cb7a5156cb
Halo-Mango <04austin.jpeg@gmail.com>
2026-03-30 23:48:38 -0500

    Update SimulationTest.java

--- src/test/java/com/example/SimulationTest.java
+++ src/test/java/com/example/SimulationTest.java
@@ -9,24 +9,26 @@ import org.junit.jupiter.api.Test;
 import java.util.Random;
 
 public class SimulationTest {
-
+    // This test is to check if a herbivore eats a plant if the plant is next to it
     @Test
     public void testHerbivoreEatsPlant() {
+        //creates a 2x1 world
         World world = new World(2, 1);
+        // herb location at axis (0,0) and plants
         Herbivore herbivore = new Herbivore(0, 0);
         Plant plant = new Plant(1, 0);
-
+        // adds them to the world
         world.add(herbivore);
         world.add(plant);
 
         Random rng = new Random(1);
         herbivore.act(world, rng);
 
-        // After eating, plant should be gone
+        // After eating the plant should be gone
         assertNull(world.get(0, 0));
         assertSame(herbivore, world.get(1, 0));
     }
-
+    // This test will check if a herbivore will eat another herbivore
     @Test
     public void testHerbivoreDoesNotEatHerbivore() {
         World world = new World(2, 2);
@@ -39,10 +41,10 @@ public class SimulationTest {
         Random rng = new Random(1);
         h1.act(world, rng);
 
-        // Second herbivore should still exist
+        // this to see if the other herbivore still lives
         assertNotNull(world.get(h2.getX(), h2.getY()));
     }
-
+    // Checks that a herbivore will die once its energy has reached zero
     @Test
     public void testHerbivoreDiesAfterEnergyZero() {
         World world = new World(1, 1);
@@ -58,7 +60,8 @@ public class SimulationTest {
 
         assertTrue(herbivore.isDead());
     }
-
+    
+    // Will check if a carnivore will die after its energy has reached zero.
     @Test
     public void testCarnivoreDiesAfterEnergyZero() {
         World world = new World(1, 1);
